bbdb88483e4a43d6a1840f91864992a12f89586c
Criotech <opesiyanbola8991@gmail.com>
2019-01-06 22:46:44 +0100

    admin dashboard

---
+++ models/users.js
@@ -0,0 +1,26 @@
+let mongoose = require("mongoose");
+const bcrypt = require("bcrypt-nodejs")
+
+let Schema = mongoose.Schema;
+
+
+let userSchema = new Schema({
+    name: {type: String},
+    email: {type: String, trim: true},
+    password: {type: String},
+    position: {type: String},
+    createdDate: {
+        type: Date,
+        default: Date.now
+    }
+})
+
+userSchema.methods.generateHash = function(password){
+    return bcrypt.hashSync(password, bcrypt.genSaltSync(10))
+}
+
+userSchema.methods.validatePassword = function(password){
+    return bcrypt.compareSync(password, this.password)
+}
+
+module.exports = mongoose.model("User", userSchema)
