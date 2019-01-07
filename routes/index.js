2d9b76dc19477fd0c11bc984c8291c34cd5bc5a4
Criotech <opesiyanbola8991@gmail.com>
2019-01-07 20:09:44 +0100

    update on slider

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,10 @@
+const path = require('path');
 var express = require('express');
 var router = express.Router();
 let passport = require("passport");
+const multer =require("multer");
 let User = require('../models/users');
+let Slider = require('../models/slider');
 const methodOverride = require("method-override");
 const controller = require('../controller')
 let dashboardController = require('../controllers/dashboard-controllers.js');
@@ -79,6 +82,133 @@ router.delete('/deleteadmin', function(req, res, next){
     })
 
 })
+
+//slider
+router.get('/dashboard/slider', function(req, res, next){
+      let failure = req.flash('failure');
+      let success = req.flash('success');     
+
+   Slider.find({}).then((result)=>{
+    if (result){
+       res.render('slider', {result, failure, success})
+      console.log(result)
+    }else{
+       res.render('slider')      
+    }
+  })
+})
+
+const  storage = multer.diskStorage({
+  destination: './public/uploads',
+  filename: function(req, file, cb){
+    cb(null, file.fieldname + "-" + Date.now() + path.extname(file.originalname));
+  }
+})
+
+
+const upload = multer({
+  storage: storage ,
+  //limits: {fileSize: 10},
+  fileFilter: function(req, file, cb){
+    checkFileType(file, cb);
+  }
+}).fields([{name: "slider1"},{name: "slider2"},{name: "slider3"}])
+
+//check file type 
+function checkFileType(file, cb){
+  //Allowed ext
+  const filetypes = /jpeg|jpg|png|gif/;
+  // check ext
+  const extname = filetypes.test(path.extname
+  (file.originalname).toLowerCase());
+  //check mime
+  const mimetype = filetypes.test(file.mimetype)
+
+  if(mimetype && extname){
+    return cb(null, true);
+  }else {
+    cb('Error: images Only!')
+  }
+}
+
+router.post("/uploadslider", function (req, res){
+
+  upload(req, res, (err) => {
+    if (err){
+    
+    //res.render('students', {msg : err})
+   res.send(err)
+    }else{
+      console.log(req.files);
+      Slider.find({}).then(function(result){
+   if (!result){ 
+
+        let newSlider = new Slider();
+           newSlider.slider1.name = req.files['slider1'][0].fieldname;
+           newSlider.slider1.path = req.files['slider1'][0].path;
+           newSlider.slider2.name = req.files['slider2'][0].fieldname;
+           newSlider.slider2.path = req.files['slider2'][0].path;
+           newSlider.slider3.name = req.files['slider3'][0].fieldname;
+           newSlider.slider3.path = req.files['slider3'][0].path;
+           newSlider.name = "slider";           
+           
+         newSlider.save().then((result)=>{
+           if(result){
+             console.log(result)
+             res.redirect("/dashboard/slider");
+           }else{
+             res.send("err")
+           }
+         })
+
+         }else{
+               req.flash('failure', "Sorry You can only update sliders not create new ones");
+             res.redirect("/dashboard/slider");
+
+    // console.log("sorry cannot save new data")
+   }   
+     // res.send("test")
+    })
+    }
+  })
+})
+
+
+router.put("/update/uploadslider", function (req, res){
+
+  upload(req, res, (err) => {
+    if (err){
+    
+    //res.render('students', {msg : err})
+   res.send(err)
+    }else{
+      console.log(req.files);
+      Slider.findOneAndUpdate({"name": "slider"},
+       {$set:{"slider1.name": req.files['slider1'][0].fieldname,
+        "slider1.path": req.files['slider1'][0].path,
+        "slider2.name": req.files['slider2'][0].fieldname,
+      "slider2.path": req.files['slider2'][0].path,
+     "slider3.name": req.files['slider3'][0].fieldname,
+     "slider3.path": req.files['slider3'][0].path,
+    
+    }},
+     {new: true})
+      .then((result)=>{
+          if (result) {
+               req.flash('success', "Slider has been updated");            
+            res.redirect("/dashboard/slider")
+          } else {
+            res.send("error")
+          }
+      })
+       
+      
+     // res.send("test")
+    }
+  })
+})
+
+
 
 router.post('/createAccount', passport.authenticate('local.registerAdmin',{
   successRedirect: '/dashboard/authorizeadmins',
