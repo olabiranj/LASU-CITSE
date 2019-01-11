3278dd9351574f42d55593e0127df37b21c3510b
Criotech <opesiyanbola8991@gmail.com>
2019-01-11 23:25:58 +0100

    moned to m-lab

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ require("./config/passport");
 
 var app = express();
 
-mongoose.connect('mongodb://localhost:27017/dashboard', { useNewUrlParser: true }).then(console.log("database connected"));
+mongoose.connect('mongodb://criotech:lasu123@ds255364.mlab.com:55364/criotech', { useNewUrlParser: true }).then(console.log("database connected")).catch(err=>console.log(err));
 
 
 // view engine setup
@@ -72,5 +72,7 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('frontend/error');
 });
+
+
 
 module.exports = app;
