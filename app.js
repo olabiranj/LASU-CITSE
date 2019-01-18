dc182c1a622108cc9e1d66dc9984f2f3111745a6
AbdRozaq Iwashokun <ade.iwashokun@triatronltd.com>
2019-01-18 11:07:39 +0100

    code update

--- app.js
+++ app.js
@@ -13,8 +13,6 @@ const flash = require("express-flash");
 const multer =require("multer");
 const methodOverride = require("method-override");
 const nodemailer = require("nodemailer")
-// const bootstrap = require("bootstrap")
-// const jquery = require(jquery)
 
 
 var indexRouter = require('./routes/index');
@@ -30,7 +28,6 @@ mongoose.connect(db_uri, { useNewUrlParser: true }).then(console.log("database c
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-// app.set('views', path.join(__dirname, 'views/partials'));
 
 
 app.set('view engine', 'ejs');
@@ -40,18 +37,16 @@ app.use(methodOverride('_method'))
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(bodyParser.urlencoded({extended: true }));
-app.use(bodyParser.json());
+app.use(cookieParser('mysecrect'));
 
 app.use(express.static(path.join(__dirname, 'public')));
 
 
 app.use(session({
-  secret: "mysecrect",
-  resave: true,
-  saveUninitialized: true,
-  store: new MongoStore({uri: db_uri, collection: 'app_sessions'})
+    secret: "mysecrect",
+    resave: true,
+    saveUninitialized: true,
+    store: new MongoStore({uri: db_uri, collection: 'app_sessions'})
 }));
 
 app.use(flash());
@@ -64,18 +59,18 @@ app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  next(createError(404));
+    next(createError(404));
 });
 
 // error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('frontend/error');
+    // render the error page
+    res.status(err.status || 500);
+    res.render('frontend/error');
 });
 
 
