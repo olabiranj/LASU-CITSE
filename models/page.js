755809804ad734f616ddbdd2c6cd555848365070
AbdRozaq Iwashokun <ade.iwashokun@triatronltd.com>
2019-01-18 11:05:13 +0100

    page model updated

--- models/page.js
+++ models/page.js
@@ -1,17 +1,27 @@
 let mongoose = require("mongoose");
-
 let Schema = mongoose.Schema;
 
-
 let PageSchema = new Schema({
-    newImg: { type: String },    
-    content: { type: String },
-    name: {type: String}, 
+    tag: {
+        type: String
+    },
+    name: {
+        type: String
+    },
+    content: {
+        type: String
+    },
+    postImage: {
+        type: String
+    },
+    is_active: {
+        type: Boolean,
+        default: true
+    },
     createdDate: {
         type: Date,
         default: Date.now
     }
 })
-
 
 module.exports = mongoose.model("Page", PageSchema)
