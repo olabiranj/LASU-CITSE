b3fafb9c0c351c5203dd200eb4d38fd9500ef832
AbdRozaq Iwashokun <ade.iwashokun@triatronltd.com>
2019-01-19 10:57:33 +0100

    all frontend pages integrated, new logo, 404 page

--- routes/index.js
+++ routes/index.js
@@ -85,28 +85,8 @@ router.get('/services', controller.servicesPage);
 router.get('/contact', controller.contactPage);
 router.post('/post_contact', controller.post_contactPage);
 router.get('/team', controller.teamPage);
-router.get('/justification-for-establishing-the-centre', controller.jecPage);
-router.get('/vision', controller.visionPage);
-router.get('/mission', controller.missionPage);
-router.get('/objectives', controller.objectivesPage);
-router.get('/retention-and-support', controller.retentionPage);
-router.get('/student-recruitment', controller.recruitmentPage);
-router.get('/staff-development', controller.staffPage);
-router.get('/centre-operations', controller.operationsPage);
-router.get('/education', controller.educationPage);
-router.get('/learning', controller.learningPage);
-router.get('/teaching', controller.teachingPage);
-router.get('/skills', controller.skillsPage);
-router.get('/innovation', controller.innovationPage);
-router.get('/innovation', controller.innovationPage);
-router.get('/innovationAss', controller.innovationAssPage);
-router.get('/onlineCourse', controller.onlineCoursePage);
-router.get('/researchPlan', controller.researchPlanPage);
-router.get('/industrial', controller.industrailPage);
-router.get('/partnership', controller.partnershipPage);
-router.get('/implementation-timeline', controller.implementationPage);
 router.get('/news', controller.newsPage);
-
+router.get('/:page_name', controller.renderPage);
 
 // DASHBOARD ROUTES
 // -----
